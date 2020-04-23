4aa2e966f1ce6781b92fda9fbb1e8db59fd84098
jonathan <josagal.ing@gmail.com>
2020-04-23 11:13:07 -0500

    programa básico con turtle

---
+++ turtle_square.py
@@ -0,0 +1,20 @@
+import turtle
+
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90
+        )
+tortuga.forward(100)
+tortuga.right(90)
+
+window.mainloop()
+
